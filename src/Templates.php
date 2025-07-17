01e42fc8774c2cd993305b62433479c63b346bb0
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2025-07-17 17:17:56 +0100

    add a method to get the path of the templates

--- src/Templates.php
+++ src/Templates.php
@@ -8,12 +8,17 @@ class Templates
         //
     }
 
+    public function getPath(): string
+    {
+        return path_join($this->plugin->getPath(), "views/");
+    }
+
     public function render(string $template, array $data = []): string
     {
         extract($data);
         ob_start();
 
-        include path_join($this->plugin->getPath(), "views/$template.php");
+        include path_join($this->getPath(), "$template.php");
 
         return ob_get_clean();
     }
